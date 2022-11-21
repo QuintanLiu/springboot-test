469caa67cfafce3d8dee68ed4455949a1ee9d650
QuintanLiu <718526027@163.com>
2022-11-21 10:24:28 +0000

    Pending changes exported from your codespace

---
+++ kunkun-test/src/test/java/com/kunkun/boot/kunkuntest/KunkunTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunkun.boot.kunkuntest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunkunTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
